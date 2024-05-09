3cd1c4e856d3368f121c1c604d4d585799fd66c0
tonsukirin <sukirintlbcc@gmail.com>
2024-05-09 22:49:32 +0700

    font fix again

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,5 @@
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
 import './globals.css';
-
-const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'Social Media Image Captioning',
@@ -19,7 +16,7 @@ export default function RootLayout({
       <head>
         <meta charSet="UTF-8" />
       </head>
-      <body className={inter.className}>{children}</body>
+      <body className="font-sans">{children}</body>
     </html>
   );
 }
